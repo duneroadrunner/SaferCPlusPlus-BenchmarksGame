078e3522bc4259882410c798ef7ff559704fdc55
yyyy <yyyy@yyyy>
2016-10-25 14:25:05 -0700

    initial commit

---
+++ binary-trees-indices/binary-trees-indices_u64q_unchecked/gppbuild_binary-trees-indices_u64q_unchecked.sh
@@ -0,0 +1,1 @@
+/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -fopenmp -lboost_system -DNDEBUG binary-trees-indices_u64q_unchecked.cpp -o binary-trees-indices_u64q_unchecked.gpp_run
