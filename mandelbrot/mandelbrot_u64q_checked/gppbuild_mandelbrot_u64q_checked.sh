9074204ae532a7de238a07d8aa217ee1805b9855
yyyy <yyyy@yyyy>
2016-10-28 16:22:38 -0700

    initial commit

---
+++ mandelbrot/mandelbrot_u64q_checked/gppbuild_mandelbrot_u64q_checked.sh
@@ -0,0 +1,1 @@
+/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -fopenmp -lboost_system -DNDEBUG -I../../msetl mandelbrot_u64q_checked.cpp -o mandelbrot_u64q_checked.gpp_run
