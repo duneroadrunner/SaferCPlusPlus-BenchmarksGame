4212165dc4e9c8daba339590557ea66ce6ed6b6c
yyyy <yyyy@yyyy>
2016-11-05 06:45:02 -0700

    initial commit

---
+++ spectral-norm/spectral-norm_u64q_strict/gppbuild_spectral-norm_u64q_strict.sh
@@ -0,0 +1,1 @@
+/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -fopenmp -lboost_system -DNDEBUG -I../../msetl spectral-norm_u64q_strict.cpp -o spectral-norm_u64q_strict.gpp_run
