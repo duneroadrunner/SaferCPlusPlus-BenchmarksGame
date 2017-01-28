5daa3a535a7cb3aae17416589705ad7fe1d78a47
yyyy <yyyy@yyyy>
2017-01-28 09:04:39 -0800

    removed unnecessary boost dependency

--- fasta/fasta_u64q_checked/gppbuild_fasta_u64q_checked.sh
+++ fasta/fasta_u64q_checked/gppbuild_fasta_u64q_checked.sh
@@ -1,1 +1,1 @@
-/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -fopenmp -lboost_system -DNDEBUG -I../../msetl fasta_u64q_checked.cpp -o fasta_u64q_checked.gpp_run
+/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -pthread -DNDEBUG -I../../msetl fasta_u64q_checked.cpp -o fasta_u64q_checked.gpp_run
