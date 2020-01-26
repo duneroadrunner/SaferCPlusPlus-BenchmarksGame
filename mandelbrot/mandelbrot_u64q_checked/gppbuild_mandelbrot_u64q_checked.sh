3b8f817380e5a8c9d38fe77d099d0362aaa5f56c
x <x@y.com>
2020-01-26 10:41:13 -0800

    removed "checked" implementations

--- mandelbrot/mandelbrot_u64q_checked/gppbuild_mandelbrot_u64q_checked.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -DNDEBUG -I../../msetl mandelbrot_u64q_checked.cpp -o mandelbrot_u64q_checked.gpp_run
--- spectral-norm/spectral-norm_u64q_checked/gppbuild_spectral-norm_u64q_checked.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/g++ -pipe -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3  -std=c++14 -fopenmp -DNDEBUG -I../../msetl spectral-norm_u64q_checked.cpp -o spectral-norm_u64q_checked.gpp_run
